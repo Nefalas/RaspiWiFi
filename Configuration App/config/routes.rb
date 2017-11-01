c040d03c3617073df10d8d597de9a4ae733def9c
Alexis Lindgren <alexisli@kth.se>
2017-11-01 15:09:44 +0100

    Added Feedo stuff

--- Configuration App/config/routes.rb
+++ Configuration App/config/routes.rb
@@ -16,19 +16,15 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-   root 'main#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-     get 'main/save_credentials' => 'main#save_credentials'
-     get 'main/reset_all' => 'main#reset_all'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-     resources :main
 
   # Example resource route with options:
   #   resources :products do
